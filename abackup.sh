cc6bed3037ec203f8bcda57f692d554d94f7c836
Derk Muenchhausen <derk.muenchhausen@siemens.com>
2019-05-11 20:32:48 +0200

    feat: diff backup

--- abackup.sh
+++ abackup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exit on error
+set -e
+
 OTHER=()
 while [[ $# -gt 0 ]]
 do
@@ -8,6 +11,16 @@ key="$1"
 case $key in
     -h|--help)
     HELP="1"
+    shift
+    ;;
+    --destfolder_diff)
+    DESTFOLDER_DIFF=("$2")
+    shift
+    shift
+    ;;
+    --destfolder_rolling)
+    DESTFOLDER_ROLLING=("$2")
+    shift
     shift
     ;;
     status)
@@ -33,15 +46,31 @@ fi
 
 source backup.cfg
 
+if [[ -n $DESTFOLDER_DIFF ]]; then
+    destfolder_diff=$DESTFOLDER_DIFF
+fi
+if [[ -n $DESTFOLDER_ROLLING ]]; then
+    destfolder_rolling=$DESTFOLDER_ROLLING
+fi
+
+
 if [[ -n $1 ]]; then
     echo "additional arguments:" $1
 fi
 
 if [[ -n $RUN ]]; then
-    rsync ${rsyncargs} ${sourcefolder} ${destfolder}
-    echo $?
+    echo "****** rolling backup *******"
+    mkdir -p ${destfolder_rolling}
+    rsync ${rsyncopts} "${sourcefolder}" "${destfolder_rolling}"
+    
+    echo "***** differential backup ******"
+    destfolder_diff_last=$(realpath -m --relative-to=. ${destfolder_diff}..)/last
+    echo destfolder_diff_last $destfolder_diff_last
+    mkdir -p ${destfolder_diff}
+    rsync ${rsyncopts}  "${sourcefolder}"  "${destfolder_diff}" --link-dest="${destfolder_diff_last}"
+    ln -nsf "${destfolder_diff}" "${destfolder_diff_last}"
 fi
 
 if [[ -n $STATUS ]]; then
-    echo status
+    echo status: not implemented
 fi
